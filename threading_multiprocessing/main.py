37ff54dea7520a6544b442c19f24276d1f306508
Im-Stivik <mogil.sharon@gmail.com>
2025-07-29 15:28:52 +0300

    added writing to csv

--- threading_multiprocessing/main.py
+++ threading_multiprocessing/main.py
@@ -1,5 +1,16 @@
 from datetime import datetime
+from typing import Iterable, Dict, Any
+
 from src.stock_retriever import StockRetriever
+import csv
+
+def write_output_to_csv(data: Iterable[Dict[str, Any]], filename: str):
+    with open(filename, 'w', newline='') as csvfile:
+        fieldnames = ['timestamp', 'stock', 'percentage_change']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+        writer.writerows(data)
 
 def main():
     stock = StockRetriever('BTC-USD')
@@ -7,8 +18,8 @@ def main():
     starting_time = datetime.fromisoformat('2025-03-17 19:00')
     ending_time = datetime.fromisoformat('2025-06-15 13:00')
 
-    test = list(stock.get_stock_for_timestamp(starting_time, ending_time))
-    print(test)
+    data = stock.get_stock_for_timestamp(starting_time, ending_time)
+    write_output_to_csv(data, 'output.csv')
 
 
 if __name__ == '__main__':
